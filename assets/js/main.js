5b7f72d68981720fcdc577e51a222895b6078c0d
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2022-05-27 18:09:54 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,16 @@ function initBattery(){
     navigator.getBattery().then((batt) =>{
         updateBattery = () =>{
             /* 1. We update the number level of the battery */
+            /* 1. Atualiza o nível de número da bateria */
             let level = Math.floor(batt.level * 100)
             batteryPercentage.innerHTML = level+ '%'
 
             /* 2. We update the background level of the battery */
+            /* 2. Atualiza o nível de fundo da bateria */
             batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`
 
             /* 3. We validate full battery, low battery and if it is charging or not */
+            /* 3. Valida se a bateria cheia, bateria fraca, se está carregando ou não */
             if(level == 100){ /* We validate if the battery is full */
                 batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`
                 batteryLiquid.style.height = '103%' /* To hide the ellipse */
@@ -31,6 +34,7 @@ function initBattery(){
             }
             
             /* 4. We change the colors of the battery and remove the other colors */
+            /* 4. Muda as cores da bateria e removemos as outras cores */
             if(level <=20){
                 batteryLiquid.classList.add('gradient-color-red')
                 batteryLiquid.classList.remove('gradient-color-orange','gradient-color-yellow','gradient-color-green')
@@ -51,6 +55,7 @@ function initBattery(){
         updateBattery()
 
         /* 5. Battery status events */
+        /* 5. Status da bateria */
         batt.addEventListener('chargingchange', () => {updateBattery()})
         batt.addEventListener('levelchange', () => {updateBattery()})
     })
